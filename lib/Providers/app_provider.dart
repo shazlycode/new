f2bce14be27d9981debb13811a5a8ef997d03fec
drahm <dr.ahmedshazly@yahoo.com>
2024-10-14 08:05:33 +0300

    2nd deployment

--- lib/Providers/app_provider.dart
+++ lib/Providers/app_provider.dart
@@ -51,6 +51,15 @@ class AppProvider with ChangeNotifier {
             "https://play-lh.googleusercontent.com/MsytCWjFUVztaED_N8dc8Bka1cLEvuGmnzoWlYTq_19eIY4xpcpyM8vomH1Q-CzoZUxM=w240-h480-rw",
         url:
             "https://play.google.com/store/apps/details?id=com.shazlycode.bmi"),
+    App(
+        id: "5",
+        title: "القران الكريم| The Holly Quran",
+        description:
+            "With our app, you can now listen to the Quran on-the-go, at home, or anywhere you like. Our app features high-quality audio recitations from renowned Quran reciters, ensuring an authentic and immersive listening experience.",
+        image:
+            "https://play-lh.googleusercontent.com/Op025DyPRfWoRk-6O0PvF3x5z4emq8Lirh3-JW0PESrASZ73vtQ7hzVa2ajQHsTkBjGG=w240-h480-rw",
+        url:
+            "https://play.google.com/store/apps/details?id=com.shazlycode.quran_app_test"),
   ];
   List<App> get apps => _apps;
 }
