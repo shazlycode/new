1878308a9315f1fa8174aa89bf1e187d94e3eecb
Shazlycode <53357640+shazlycode@users.noreply.github.com>
2024-10-16 01:32:57 +0300

    Update tablet_layout.dart

--- lib/Layouts/Tablet Layout/tablet_layout.dart
+++ lib/Layouts/Tablet Layout/tablet_layout.dart
@@ -6,7 +6,7 @@ class TabletLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text("Tablet Layout from branch two"),
+      child: Text("Tablet Layout from branch two and this update from the remote"),
     );
   }
 }
